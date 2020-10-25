4c16b3021b9124125dc7227c8b9ef54d570c65c7
Brandon Leong <bjtl1105@gmail.com>
2020-10-25 11:09:59 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require('./models/User')
+const bodyParser = require('body-parser');
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+
+app.get("/", (req, res) => { 
+    const user = new User({
+        handle: "Josh",
+        email: "JYBright@liberl.orb",
+        password: "EyesOfAmber"
+    })
+    user.save()
+    res.send("Hello");
+});
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
